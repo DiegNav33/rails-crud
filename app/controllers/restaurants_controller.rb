10deade4ae541194a9550cdfe2241a4c746b3dcc
Diego Navarro <137430006+DiegNav33@users.noreply.github.com>
2024-07-30 16:33:38 +0800

    Few updates

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,11 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -20,18 +20,15 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
     # No need for app/views/restaurants/update.html.erb
     redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     # No need for app/views/restaurants/destroy.html.erb
     redirect_to restaurants_path, status: :see_other #L'utilisation de status: :see_other est généralement recommandée pour les redirections après des opérations qui modifient les données (POST, DELETE) afin de garantir une gestion correcte des redirections et éviter des problèmes comme la re-soumission de formulaires.
@@ -39,6 +36,10 @@ class RestaurantsController < ApplicationController
 
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :rating)
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,29 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
+  # get 'restaurants/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "restaurants", to: "restaurants#index", as: :restaurants
-  # The `new` route needs to be placed before the `show` route
-  get "restaurants/new", to: "restaurants#new", as: :new_restaurant
-  post "restaurants", to: "restaurants#create"
-  get "restaurants/:id", to: "restaurants#show", as: :restaurant
-  get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
-  patch "restaurants/:id", to: "restaurants#update"
-  delete "restaurants/:id", to: "restaurants#destroy"
+  # get "up" => "rails/health#show", as: :rails_health_check
+
+  # # Lire tous les restaurants
+  # get "restaurants", to: "restaurants#index", as: :restaurants
+
+  #  # Créer un nouveau restaurant
+  # get "restaurants/new", to: "restaurants#new", as: :new_restaurant
+  # post "restaurants", to: "restaurants#create"
+
+  #  # Lire un restaurant spécifique (cette route doit être après la route `new`)
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  # # Mettre à jour un restaurant
+  # get "restaurants/:id/edit", to: "restaurants#edit", as: :edit_restaurant
+  # patch "restaurants/:id", to: "restaurants#update"
+
+  # # Supprimer un restaurant
+  # delete "restaurants/:id", to: "restaurants#destroy"
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants
 end
